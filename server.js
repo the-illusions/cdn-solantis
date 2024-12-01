2806e3d82ff79b874e0b9ae5b713b6d9002878df
the-illusions <the.illusion.dev@gmail.com>
2024-12-01 22:30:22 +0600

    V1.0.1 First Release

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(helmet());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
+// Security Middleware for image access
+app.get('/cdn-image', (req, res) => {
+    const referer = req.headers.referer || '';
+    const allowedDomain = process.env.ALLOWED_DOMAIN;
+
+    if (referer.startsWith(allowedDomain)) {
+        res.sendFile(path.join(__dirname, 'public/images/Nation-Union-Finale ZMV3.png'));
+    } else {
+        res.status(403).send('You are not authorized to access this resource.');
+    }
+});
+
+// Render frontend page
+app.get('/', (req, res) => {
+    res.render('index', { imageUrl: `${req.protocol}://${req.get('host')}/cdn-image` });
+});
+
+// Start server
+app.listen(PORT, () => console.log(`CDN running on port ${PORT}`));
