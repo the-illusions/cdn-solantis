a06111d4a49a793f448bbbf9c34d828fc628dd95
the-illusions <the.illusion.dev@gmail.com>
2024-12-01 22:46:02 +0600

    V1.0.3

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,13 +8,12 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'html');
-app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 // Security Middleware for image access
 app.get('/cdn-image', (req, res) => {
     const referer = req.headers.referer || '';
-    const allowedDomain = process.env.ALLOWED_DOMAIN;
+    const allowedDomain = process.env.ALLOWED_DOMAIN || 'https://gen-jut-su.com/solantis/';
 
     if (referer.startsWith(allowedDomain)) {
         res.sendFile(path.join(__dirname, 'public/images/Nation-Union-Finale ZMV3.png'));
